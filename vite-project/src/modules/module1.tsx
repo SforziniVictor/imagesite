16713a9b40c50e30b9b1277912405961bb01a6bd
luvhITU <luvh@itu.dk>
2024-06-19 15:00:13 +0200

    Commit some top bar

--- vite-project/src/modules/module1.tsx
+++ vite-project/src/modules/module1.tsx
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import Navbar from 'react-bootstrap/Navbar';
 
 class TopBar extends Component {
     render() {
         return (
-            <div>
-                <h1>Top Bar Component</h1>
-            </div>
+        <div>
+            <Navbar className="top-bar"
+                    style={{ background:"lightgray"}}>
+                <Navbar.Brand href="#home"
+                              style={{color: "green"}}>
+                    Home
+                </Navbar.Brand>
+                <Navbar.Brand href="#home"
+                              style={{color: "green"}}>
+                    Pricing?
+                </Navbar.Brand>
+                <Navbar.Brand href="#home"
+                              style={{color: "green"}}>
+                    HellO?
+                </Navbar.Brand>
+            </Navbar >
+            <br/>
+        </div>
         );
     }
 }  
